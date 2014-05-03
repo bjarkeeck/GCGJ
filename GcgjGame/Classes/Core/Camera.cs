13caf62f6b8833842494e4ef4ffe3a7a218a5673
bjarkeck <bjarkeelias@gmail.com>
2014-05-03 15:07:49 +0200

    Camera (Hej)

---
+++ GcgjGame/Classes/Core/Camera.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GcgjGame.Classes.Core
+{
+    public class Camera2D
+    {
+        public Camera2D()
+        {
+            Zoom = 1;
+            Position = Vector2.Zero;
+            Rotation = 0;
+            Origin = Vector2.Zero;
+            Position = Vector2.Zero;
+        }
+
+        public float Zoom { get; set; }
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public Vector2 Origin { get; set; }
+
+    }
+}
