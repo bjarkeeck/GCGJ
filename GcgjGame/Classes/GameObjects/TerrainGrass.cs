6faf66ca44abf43b04379951036bd06891545f9f
mrel <pepperonipiza@hotmail.com>
2014-05-03 15:58:26 +0200

    asd

--- GcgjGame/Classes/GameObjects/TerrainGrass.cs
+++ GcgjGame/Classes/GameObjects/TerrainGrass.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using GcgjGame.Classes.Core;
+using GcgjGame.Classes.Core;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -93,39 +92,4 @@ namespace GcgjGame.Classes.GameObjects
 
     }
 }
-=======
-﻿using GcgjGame.Classes.Core;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace GcgjGame.Classes.GameObjects
-{
-    public class TerrainGrass : GameObject
-    {
-        public Texture2D Texture;
-
-        public override void Initialize(Vector2 position)
-        {
-            this.Rectangle = new Rectangle(0, 0, 1920, 1080);
-            this.Position = position;
-        }
-
-        public override void LoadContent(ContentManager content)
-        {
-            Texture = content.Load<Texture2D>("Images/Grass");
-        }
-
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            
-            spriteBatch.Draw(Texture, this.Rectangle, Color.White);
-        }
-
-    }
-}
->>>>>>> 922d2151007faecf15cf634c4cfe73617e521733
